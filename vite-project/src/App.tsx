9ca07c54c1bc67ac2091998eb3feeb68bcf3ca35
RamazanM <r.ramazanmutlu@gmail.com>
2025-10-29 15:16:24 +0300

    feat: React speech kit poc implemented.

--- vite-project/src/App.tsx
+++ vite-project/src/App.tsx
@@ -1,35 +1,45 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState, type ChangeEvent } from "react";
+import "./App.css";
+import { useSpeechSynthesis, type SpeakOptions } from "react-speech-kit";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [text, setText] = useState("");
+  const { speak, cancel } = useSpeechSynthesis();
+  const [timeoutId,setTimeoutId] = useState(0)
+  function handleTextInput(event: ChangeEvent<HTMLTextAreaElement>) {
+    const eText = event.target.value;
+    setText(eText.toLocaleUpperCase());
+    debounceSpeak(eText);
+  }
+
+  function debounceSpeak(text: string) {
+    cancel();
+    clearTimeout(timeoutId);
+    console.log("Timeout clear:"+timeoutId);
+    
+    const tId = setTimeout(() => {
+      console.log("In the timeout:"+timeoutId);
+      
+      const words = text.split(" ");
+      const sentences = text.split("\n");
+      const speakOptions: SpeakOptions = { text: "" };
+      if (sentences.length > 0)
+        speakOptions.text = sentences[sentences.length - 1];
+      else if (words.length > 0) speakOptions.text = words[words.length - 1];
+      else speakOptions.text = text[text.length - 1];
+      speak(speakOptions);
+    }, 1000);
+    setTimeoutId(tId)
+    console.log("New Timeout:"+timeoutId);
+
+  }
+
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <textarea style={{boxSizing:"border-box", width:"100%", height:"100%", fontSize:"99px"}} onChange={handleTextInput} value={text}></textarea>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
